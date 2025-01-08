611ef2fae84d7faa53f82bd84d926c82f0eaeaa1
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-08 14:35:54 +0700

    Update bot intents

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,15 @@ import {Client, Events, GatewayIntentBits} from 'discord.js'
 const token = process.env.DISCORD_TOKEN
 
 // Create a new client instance
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
